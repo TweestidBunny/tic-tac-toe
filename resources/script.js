338279a00a61d740ef5ce780c4f4d2b3df145e92
Sean <tweestidbunny@gmail.com>
2024-08-26 21:43:21 -0400

    Replace clearSquares method with makeSquares method

--- resources/script.js
+++ resources/script.js
@@ -2,8 +2,6 @@
 Create a tic-tac-toe game.
 
 What do you need:
-  Player objects:
-    Created using a Module.
   Object to add content to page:
     screenObj?
   Game flow object:
@@ -11,24 +9,43 @@ What do you need:
     Check if selected square already has content.
     Switch between Player1 and Player2.
     Check if all squares are full with no winner(a tie).
+
+    Get input from user:
+      When square is pressed, get square location:
+        querySelectorAll
+    
+  When square is clicked, get current player marker.
+  Add marker to 'square' array.
+  Update the screen with the content of the array after every 'play'.
+
+  Keep track of turns:
+    Create function to increase turn counter.
+    Create function to reset turn counter on new game.
+    Create function to retrieve turn counter.
+
+    Create 'currentPlayer' variable, tied to player1 or player2 based on turn count.
+    Create method to increase turn count, add currentPlayer marker to array.
+
+    addMarker move to gameFlow from players.
 */
 
 const gameBoard = (function () {
   const squares = [];
-  for (let i = 0; i < 9; i++) {
-    squares.push(null);
+
+  const makeSquares = function () {
+    for (let i = 0; i < 3; i++) {
+      squares.push([])
+      for (let j = 0; j < 3; j++) {
+        squares[i].push(null);
+      }
+    }
   }
 
   const getSquares = () => squares;
-  const clearSquares = function () {
-    for (let i = 0; i < 9; i++) {
-      squares[i] = null;
-    }
+  const addMarker = function (index1, index2, marker) {
+    squares[index1][index2] = marker;
   }
-  const addMarker = function (index, marker) {
-    squares[index] = marker;
-  }
-  return { getSquares, clearSquares, addMarker };
+  return { getSquares, makeSquares, addMarker };
 })();
 
 const players = function (name, marker) {
